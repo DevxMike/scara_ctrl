748bfad7babfa3cfdb1c16270fe09935c3e5e1e5
Mike <michalbaz@poczta.onet.pl>
2022-01-28 03:04:34 +0100

    got rid of unnecessary code

--- Inc/app.h
+++ Inc/app.h
@@ -21,15 +21,9 @@ private:
 	static float voltage;
 	static uart_wrapper uart;
 	static osSemaphoreId_t adc_sem; //adc semaphore
-	static matrix w1, w2, w3, w4; //neural network wages
-	static matrix b1, b2, b3, b4; //neural network biases
-	static double neural_net_in[3]; //normalised neural net in
-	static double neural_net_out[3]; //normalised neural net out
-	static osSemaphoreId_t neural_sem; //neural net semaphore
 
 	static void input_check(void* p);
 	static void manage_lcd(void* p);
-	static void neural_net_task(void* p);
 
 public:
 	static osSemaphoreId_t input_sem; //input semaphore
--- Src/app.cpp
+++ Src/app.cpp
@@ -14,8 +14,6 @@ float app::voltage{ 0.0f };
 
 uart_wrapper app::uart{ &app::uart_handle };
 
-double app::neural_net_in[3]{ -0.200430, 0.391892, - 1.647278 };
-double app::neural_net_out[3]{ 0.0 };
 
 const osThreadAttr_t lcd_task_attr = {
 	.name = "lcd task",
@@ -27,13 +25,8 @@ const osThreadAttr_t input_check_attr = {
 	.priority = osPriorityLow
 };
 
-const osThreadAttr_t nn_task_attr = {
-	.name = "nn task",
-	.priority = osPriorityLow,
-};
 
 osSemaphoreId_t app::adc_sem{ osSemaphoreNew(1, 1, NULL) };
-osSemaphoreId_t app::neural_sem{ osSemaphoreNew(1, 1, NULL) };
 osSemaphoreId_t app::input_sem{ osSemaphoreNew(1, 1, NULL) };
 
 
@@ -132,101 +125,3 @@ void app::input_check(void * p){
 	}
 	osThreadTerminate(NULL);
 }
-
-void app::neural_net_task(void * p){
-	while(1){
-		matrix d( 3, 1, app::neural_net_in );
-		auto a1 = w1 * d;
-		a1 = a1 + b1;
-		matrix::element_wise_fn(a1, tanh);
-
-		osDelay(1);
-
-		auto a2 = w2 * a1;
-		a2 = a2 + b2;
-		matrix::element_wise_fn(a2, tanh);
-
-		osDelay(1);
-
-		auto a3 = w3 * a2;
-		a3 = a3 + b3;
-		matrix::element_wise_fn(a3, tanh);
-
-		osDelay(1);
-
-		auto a4 = w4 * a3;
-		a4 = a4 + b4;
-		matrix::element_wise_fn(a4, matrix::logsig);
-		app::neural_net_out[0] = a4[0];
-		app::neural_net_out[1] = a4[1];
-		app::neural_net_out[2] = a4[2];
-
-		osDelay(50);
-	}
-	osThreadTerminate(NULL);
-}
-
-double t1[]{
-	-0.48466888,  -0.02772117,  -0.56858855,
-	-0.71588149, 0.09251538 , 0.36220800,
-	0.12139439,  0.52683263,  -0.07512999
-};
-matrix app::w1{ 3, 3, t1 };
-double t2[]{
-	-0.13023305,
-	0.05342775,
-	-0.54410190
-};
-matrix app::b1{ 3, 1, t2 };
-double t3[]{
-	1.19703017, 4.95831115, -0.92103620,
-	0.78644863, 2.92063664, -0.54997715,
-	-1.04869871, 1.63298993, -0.37941664 ,
-	0.38132462, 0.82491510, 1.09168423,
-	-0.33781541, -0.10879320, 1.40038886 ,
-	1.97207363, -0.17483729, 0.08332292 ,
-	1.44167371, -0.01253153, -0.39216339
-};
-matrix app::w2{ 7, 3, t3 };
-double t4[]{
-	-0.30463793,
-	1.50417816,
-	-0.00412936,
-	-0.03696238,
-	0.44088420,
-	-1.44050641,
-	1.31509051
-};
-matrix app::b2{ 7, 1, t4 };
-double t5[]{
-	-2.71311412, 1.32878499, 5.21361655, -0.47754644, 0.12236628, 1.64632762, -1.66452792,
-	0.00676326, 0.19924505, -0.10010434, 1.33548757, 2.35509182, 0.12378472, 0.44082493,
-	-0.29383685, 0.99806483, 0.77177001, 0.10271539, -0.10759323, 0.00034232, 0.17381803,
-	1.53857986, 0.12035402, -1.02363730, 0.53038117, -0.31404730, 0.45138702, 0.42398475,
-	0.02633888, -0.49483457, 2.34180014, 1.28707211, -1.02766281, 3.91092167, 0.06803062,
-	-0.10397240, -0.08078058, -0.15021623, 2.03318847, 2.10571761, -0.09380264, -0.52113299,
-	1.64344464, 4.17683385, -0.91427208, 0.69863058, 0.51676965, -2.12814467, 1.42177840
-};
-matrix app::w3{ 7, 7, t5 };
-double t6[]{
-	-1.53971178,
-	1.20354525,
-	-1.17355805,
-	1.56110037,
-	1.84535518,
-	-0.61083426,
-	0.30657361
-};
-matrix app::b3{ 7,1, t6 };
-double t7[]{
-	-0.03190653, 2.90133833, 0.11088433, 0.06109504, -0.03959719, 3.61028087, 0.14707115,
-	4.38583105, 0.06082046, 0.73061741, 4.64518478, 2.20166683, -0.05391765, -1.64402204,
-	-0.07215400, 0.03850719, 2.44910097, -2.36970837, -4.35487053, 0.07770238, 3.61526287
-};
-matrix app::w4{ 3, 7, t7 };
-double t8[]{
-	0.85795074,
-	3.80410709,
-	-4.30926582
-};
-matrix app::b4{ 3, 1, t8 };
